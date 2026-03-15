07c4bb559e13569de2057db02da0241b81ae4284
Breanna Repluk <fxtech95@gmail.com>
2026-03-15 18:58:21 -0400

    express and middleware setup

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ import express from "express";
 const app = express();
 
 import morgan from "morgan";
+import { nanoid } from "nanoid";
+
+let trades = [
+  { id: nanoid(), ticker: "TSLA", side: "CALL" },
+  { id: nanoid(), ticker: "GOOGL", side: "PUT" },
+];
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -18,6 +24,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "Data received", data: req.body });
+});
+
+app.get("/api/v1/trades", (req, res) => {
+  res.status(200).json({ trades });
 });
 
 const port = process.env.PORT || 5100;
