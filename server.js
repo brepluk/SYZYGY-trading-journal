2d4372d12e912139f26a925716b248e47f205625
Breanna Repluk <breanna.repluk@gmail.com>
2026-03-17 17:42:17 -0400

    get all jobs, create job, get single job, edit job, delete job

--- server.js
+++ server.js
@@ -26,8 +26,59 @@ app.post("/", (req, res) => {
   res.json({ message: "Data received", data: req.body });
 });
 
+// GET ALL TRADES
 app.get("/api/v1/trades", (req, res) => {
   res.status(200).json({ trades });
+});
+
+// CREATE TRADE
+app.post("/api/v1/trades", (req, res) => {
+  const { ticker, side } = req.body;
+  if (!ticker || !side) {
+    return res.status(400).json({ message: "Ticker and side are required" });
+  }
+  const id = nanoid(10);
+  const trade = { id, ticker, side };
+  trades.push(trade);
+  res.status(200).json({ trade });
+});
+
+// GET SINGLE TRADE
+app.get("/api/v1/trades/:id", (req, res) => {
+  const { id } = req.params;
+  const trade = trades.find((trade) => trade.id === id);
+  if (!trade) {
+    return res.status(404).json({ message: `no trade found with id ${id}` });
+  }
+  res.status(200).json({ trade });
+});
+
+// EDIT TRADE
+app.patch("/api/v1/trades/:id", (req, res) => {
+  const { ticker, side } = req.body;
+  if (!ticker || !side) {
+    return res.status(400).json({ message: "Ticker and side are required" });
+  }
+  const { id } = req.params;
+  const trade = trades.find((trade) => trade.id === id);
+  if (!trade) {
+    return res.status(404).json({ message: `no trade found with id ${id}` });
+  }
+  trade.ticker = ticker;
+  trade.side = side;
+  res.status(200).json({ message: "Trade updated successfully", trade });
+});
+
+// DELETE TRADE
+app.delete("/api/v1/trades/:id", (req, res) => {
+  const { id } = req.params;
+  const trade = trades.find((trade) => trade.id === id);
+  if (!trade) {
+    return res.status(404).json({ message: `no trade found with id ${id}` });
+  }
+  const newTrades = trades.filter((trade) => trade.id !== id);
+  trades = newTrades;
+  res.status(200).json({ message: "Trade deleted successfully" });
 });
 
 const port = process.env.PORT || 5100;
